0ff09b4810744a0f39f64ce8b2c2dda219da4b9d
Daniel Ringwalt <dringw@gmail.com>
2012-09-03 15:50:06 -0400

    Updated PartStaff and Voice to allow creation from either a Measure or Part.

--- partstaff.js
+++ partstaff.js
@@ -2,20 +2,20 @@
 // Copyright (c) 2012 Daniel Ringwalt
 //
 // PartStaff - Represents a set of notes drawn on the same staff, which may span
-// multiple Measures and/or Voices.
+// multiple Measures and/or Voices. May be created from an entire Part or just
+// a single Measure of a Part.
 
-Vex.Flow.VeXML.PartStaff = function(partElement, options) {
-  if (arguments.length > 0) this.init(partElement, options);
+Vex.Flow.VeXML.PartStaff = function(parentElement, options) {
+  if (arguments.length > 0) this.init(parentElement, options);
 }
 
-Vex.Flow.VeXML.PartStaff.prototype.init = function(partStaff, options) {
+Vex.Flow.VeXML.PartStaff.prototype.init = function(parentElement, options) {
   this.options = {};
   Vex.Merge(this.options, options);
   if (! this.options.staffNum)
     throw new Error('VeXML.PartStaff requires a staffNum');
-  this.partElement = partElement;
+  this.parentElement = parentElement;
 }
 
-Vex.Flow.VeXML.Voice.prototype.getMeasure = function(measureNum, options) {
-  
+Vex.Flow.VeXML.PartStaff.prototype.getMeasure = function(measureNum, options) {
 }
--- voice.js
+++ voice.js
@@ -4,16 +4,16 @@
 // Voice - Represents a set of notes in the same voice, which may span
 // multiple Measures and/or PartStaffs.
 
-Vex.Flow.VeXML.Voice = function(partElement, options) {
-  if (arguments.length > 0) this.init(partElement, options);
+Vex.Flow.VeXML.Voice = function(parentElement, options) {
+  if (arguments.length > 0) this.init(parentElement, options);
 }
 
-Vex.Flow.VeXML.Voice.prototype.init = function(partElement, options) {
+Vex.Flow.VeXML.Voice.prototype.init = function(parentElement, options) {
   this.options = {};
   Vex.Merge(this.options, options);
   if (! this.options.voiceNum)
     throw new Error('VeXML.Voice requires a voiceNum');
-  this.partElement = partElement;
+  this.parentElement = partElement;
 }
 
 Vex.Flow.VeXML.Voice.prototype.getMeasure = function(measureNum, options) {
