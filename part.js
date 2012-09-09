8da57d3dc74af45e5b755f2c7de49847c3c5b2ea
Daniel Ringwalt <dringw@gmail.com>
2012-09-09 13:20:50 -0400

    Added multi-staff part drawing.

--- part.js
+++ part.js
@@ -27,7 +27,7 @@ Vex.Flow.VeXML.Part.prototype.getMeasure = function(measureNum, options) {
   var allMeasures = this.element.getElementsByTagName('measure');
   for (var i = 0; i < allMeasures.length; i++) {
     if (allMeasures[i].getAttribute('number') == measureNum) {
-      var measure = new Vex.Flow.VeXML.Measure(allMeasures[i]);
+      var measure = new Vex.Flow.VeXML.Measure(allMeasures[i], options);
       return measure;
     }
   }
@@ -35,6 +35,12 @@ Vex.Flow.VeXML.Part.prototype.getMeasure = function(measureNum, options) {
 
 Vex.Flow.VeXML.Part.prototype.getNumberOfStaves = function() {
   return this.getMeasure(1).getStaffNumbers().length;
+}
+
+Vex.Flow.VeXML.Part.prototype.getStaff = function(staff_num, options) {
+  var newOptions = {staff_num: staff_num};
+  Vex.Merge(newOptions, options);
+  return new Vex.Flow.VeXML.PartStaff(this, newOptions);
 }
 
 Vex.Flow.VeXML.Part.prototype.getAttributes = function(measureNum) {
@@ -52,29 +58,65 @@ Vex.Flow.VeXML.Part.prototype.engraveMeasuresOnStaves = function(
   // "staves" can be a single stave if there is only one measure
   if (! (staves instanceof Array)) {
     if (measureStart != measureEnd) {
-      throw new Error("Cannot engrave multiple measures on a single stave"); }
+      throw new Error("Cannot engrave multiple Measures on a single Stave"); }
     staves = [staves];
   }
+  var staff = this.getStaff(1);
   for (var i = 0; i < staves.length; i++) {
-    var measureNum = measureStart + i,
-        measure = this.getMeasure(measureNum),
-        notes = measure.getNotes();
-    // Only supports one staff per part
-    var stave = staves[i];
+    var measureStaves = staves[i],
+        measureNum = measureStart + i,
+        measure = new Vex.Flow.VeXML.Measure(staff.getMeasure(measureNum)),
+        voices = new Array(),
+        voiceStaves = new Array();
+    if (measureStaves instanceof Vex.Flow.Stave) {
+      measureStaves = [measureStaves];
+    }
+    for (var staveNum = 0; staveNum < measureStaves.length; staveNum++) {
+      var stave = measureStaves[staveNum];
+      var staffMeasure;
+      if (measureStaves.length == 1)
+        staffMeasure = this.getMeasure(measureNum);
+      else {
+        var staff = this.getStaff(staveNum + 1);
+        staffMeasure = new Vex.Flow.VeXML.Measure(staff.getMeasure(measureNum));
+      }
+      console.log(staffMeasure);
+      var notes = staffMeasure.getNotes();
+      var vfNotes = new Array();
+      for (var n = 0; n < notes.length; n++) {
+        vfNotes[n] = new Vex.Flow.StaveNote({ keys: notes[n].getPitches(),
+                                            duration: notes[n].getDuration() });
+      }
+      voices.push(new Vex.Flow.Voice({
+        num_beats: 4,
+        beat_value: 4,
+        resolution: Vex.Flow.RESOLUTION
+      }).setStrict(false).addTickables(vfNotes));
+      voiceStaves.push(stave);
+    }
+    var notesWidth = stave.getNoteEndX() - stave.getNoteStartX();
+    var formatter = new Vex.Flow.Formatter().joinVoices(voices);
+    formatter.format(voices, notesWidth);
+    for (var v = 0; v < voices.length; v++) {
+      voices[v].draw(context, voiceStaves[v]);
+    }
+    /*var stave = staves[i];
+    if (stave instanceof Array)
+      stave = stave[0];
     var notesWidth = stave.getNoteEndX() - stave.getNoteStartX();
     var vfNotes = new Array();
     for (var i = 0; i < notes.length; i++) {
-      vfNotes[i] = new Vex.Flow.StaveNote({ keys: [notes[i].pitch],
-                                            duration: notes[i].duration });
+      vfNotes[i] = new Vex.Flow.StaveNote({ keys: notes[i].getPitches(),
+                                            duration: notes[i].getDuration() });
     }
     var vfVoice = new Vex.Flow.Voice({
       num_beats: 4,
       beat_value: 4,
       resolution: Vex.Flow.RESOLUTION
-    });
+    }).setStrict(false);
     vfVoice.addTickables(vfNotes);
     var formatter = new Vex.Flow.Formatter().joinVoices([vfVoice]);
     formatter.format([vfVoice], notesWidth);
-    vfVoice.draw(context, stave);
+    vfVoice.draw(context, stave);*/
   }
 }
--- partstaff.js
+++ partstaff.js
@@ -12,10 +12,53 @@ Vex.Flow.VeXML.PartStaff = function(parentElement, options) {
 Vex.Flow.VeXML.PartStaff.prototype.init = function(parentElement, options) {
   this.options = {};
   Vex.Merge(this.options, options);
-  if (! this.options.staffNum)
-    throw new Error('VeXML.PartStaff requires a staffNum');
+  if (! this.options.staff_num)
+    throw new Error('VeXML.PartStaff requires a staff_num');
   this.parentElement = parentElement;
 }
 
 Vex.Flow.VeXML.PartStaff.prototype.getMeasure = function(measureNum, options) {
+  var origMeasure = this.parentElement.getMeasure(measureNum, options);
+  var clone = origMeasure.element.cloneNode(false);
+  // Need to add child node objects from original
+  var childNodes = origMeasure.element.childNodes;
+  for (var i = 0; i < childNodes.length; i++) {
+    var node = childNodes[i];
+    if (node.tagName == 'note' || node.tagName == 'direction') {
+      // Look for "staff" element matching this staff
+      var staff = node.getElementsByTagName('staff')[0];
+      if (! staff) continue;
+      if (parseInt(staff.textContent) != this.options.staff_num)
+        continue;
+      clone.appendChild(node.cloneNode(true));
+    }
+    else if (node.tagName == 'attributes') {
+      // Remove any elements that are for another staff
+      var attrElem = node.cloneNode(false);
+      var attrNodes = node.childNodes;
+      for (var j = 0; j < attrNodes.length; j++) {
+        var attrNode = attrNodes[j];
+        if (attrNode.getAttribute && attrNode.getAttribute('number') &&
+            parseInt(attrNode.getAttribute('number')) != measureNum)
+          continue;
+        attrElem.appendChild(attrNode.cloneNode(true));
+      }
+      clone.appendChild(attrElem);
+    }
+    else if (node.tagName == 'print') {
+      // Remove staff-layout elements that are for another staff
+      var printElem = node.cloneNode(false);
+      var printNodes = node.childNodes;
+      for (var j = 0; j < printNodes.length; j++) {
+        var printNode = printNodes[j];
+        if (printNode.tagName == 'staff-layout' &&
+            parseInt(printNode.getAttribute('number')) != measureNum)
+          continue;
+        printElem.appendChild(printNode.cloneNode(true));
+      }
+      clone.appendChild(printElem);
+    }
+  }
+  console.log(clone);
+  return clone;
 }
